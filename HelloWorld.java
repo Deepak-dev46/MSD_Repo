1e829a44332af309f8c60afe4c687cb6d9f634f4
Deepak M <145912399+Deepak-dev46@users.noreply.github.com>
2026-04-18 20:36:05 +0530

    new repo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld{
     public static void main(String[] args) {
 
-
+        System.out.println("https://github.com/dcs-admin/microservices.git");
 
         System.out.println("Hello World By Master Santhosh");
         System.out.println("hello i am ramesh");
